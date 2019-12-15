aab05b91e2ac0c820b9523b04ab651b0a3004726
“Nouf <nouf_mazrou@hotmail.com>
2019-12-15 10:03:24 +0300

    user login, change pass

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = require('../model/User')
 const bcrypt = require('bcrypt')
+const jwt = require ('jsonwebtoken')
 // creating a new user 
 router.post('/register', (req,res)=>{  
     
@@ -35,6 +36,55 @@ router.post('/register', (req,res)=>{
         }
     }).catch(err => res.send(err))
 })
+// nouf 
+// Login steps (1-login) 
+router.post('/login', (req, res) => {
+
+    User.findOne({ email: req.body.email
+        })
+        .then(user => {            
+            if (user) {
+                
+                if (bcrypt.compareSync(req.body.password, user.password)) {
+                    user.password = "" 
+                    var paylod = {
+                        user
+                    }
+                    let token = jwt.sign(paylod, 'secret', {
+                        expiresIn: 60*60*24*365
+                    })
+                    res.send(token)
+                }
+                // if password not the same
+                else {
+                    res.json({msg :"Password is not currect"})
+                }
+            } else {
+                // if email not exist
+                console.log("yess")
+                res.json({msg:"email is not found"})
+            }
+        })
+        .catch(err => res.send(err))
+})
+
+// change the passwoer 
+
+router.post('/changepassword/:token' , (req , res)=>{
+    
+// newPassword
+
+    var decoded = jwt.verify(req.params.token, 'secret')
+    bcrypt.hash(req.body.newPassword, 10, (err, hash) => {
+        var password = hash
+        User.findByIdAndUpdate(decoded.user._id , {password:password  }  )
+        .then(user => res.send({msg :`the password has change `  , user :user}))
+        .catch(err => res.send(err))
+    })
+ 
+
+})
+
 
 
 module.exports = router
