79632507cf7816701b32a5f8d5c7f1efb7436161
Yazeed aljuhani <yazeedmalmarwani@gmail.com>
2019-12-16 10:27:49 +0300

    deleted the file uploader because it was old

--- server.js
+++ server.js
@@ -15,65 +15,6 @@ app.use(express.urlencoded({extended:false}))
 
 
 
-// uploading file
-const { ApolloServer, gql } = require("apollo-server-express");
-const { createWriteStream, existsSync, mkdirSync } = require("fs");
-const path = require("path");
-
-const {Storage} = require('@google-cloud/storage')
-const files = [];
-
-const typeDefs = gql`
-  type Query {
-    files: [String]
-  }
-
-  type Mutation {
-    uploadFile(file: Upload!): Boolean
-  }
-`;
-
-const gc = new Storage({
-  keyFilename: path.join(__dirname, './My First Project-13b4f35e5ae3.json'),
-  projectId: 'soy-oarlock-261913'
-})
-
-const stuff = gc.bucket('hakuna-matada')
-
-const resolvers = {
-  Query: {
-    files: () => files
-  },
-  Mutation: {
-    uploadFile: async (_, { file }) => {
-      const { createReadStream, filename } = await file;
-
-      await new Promise(res =>
-        createReadStream()
-          .pipe(
-            stuff.file(filename).createWriteStream({
-              resumable : false,
-              gzip: true
-            })
-          )
-          .on("finish", res)
-      );
-
-      files.push(filename);
-
-      return true;
-    }
-  }
-};
-
-existsSync(path.join(__dirname, "../images")) || mkdirSync(path.join(__dirname, "../images"));
-
-const server = new ApolloServer({ typeDefs, resolvers });
-app.use("/images", express.static(path.join(__dirname, "../images")));
-server.applyMiddleware({ app });
-// end Uploading
-
-
 
 app.use('/user',require('./routes/user'))
 
